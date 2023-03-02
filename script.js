99bcc1177a00398968e2a3463c80263c91bc86c6
ArielDevFx <arieldevfx@gmail.com>
2023-03-02 08:52:06 -0500

    fix bugs

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function calcularNotaFinal() {
   let notaDeGrado = parseFloat(document.getElementById("notaDeGrado").value);
   let notaDeExamen = parseFloat(document.getElementById("notaDeExamen").value);
 
-  notaDeGrado = notaDeGrado * 100 * 0.75;
+  notaDeGrado = notaDeGrado * 100;
+  notaDeGrado = notaDeExamen * 0.75;
   notaDeExamen = notaDeExamen * 0.25;
 
   let notaFinal = notaDeGrado + notaDeExamen;
